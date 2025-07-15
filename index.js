b3710b1c863fa8c56d7ac0ffb2812e0c294fe8d4
Ilya Matsuev <ilya.matsuev@expereo.com>
2025-07-15 18:07:30 +0200

    Implement method find() to get specific torrent metadata

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const Parser = require("./lib/parser");
 const PageProvider = require("./lib/page-provider");
 
+function createPageProvider(host, httpClientConfigs, sessionConfigs) {
+  const pageProvider = new PageProvider(host, httpClientConfigs);
+  if (sessionConfigs.cookiesFilePath) {
+    pageProvider.storeCookies(sessionConfigs.cookiesFilePath);
+  }
+  if (
+    sessionConfigs.credentials &&
+    sessionConfigs.credentials.username &&
+    sessionConfigs.credentials.password
+  ) {
+    pageProvider.storeCredentials(
+      sessionConfigs.credentials.username,
+      sessionConfigs.credentials.password
+    );
+  }
+  return pageProvider;
+}
+
 class RutrackerApi {
-  constructor(host = "https://rutracker.org", httpClientConfigs = {}) {
+  constructor(
+    host = "https://rutracker.org",
+    httpClientConfigs = {},
+    sessionConfigs = {}
+  ) {
     this.parser = new Parser(host);
-    this.pageProvider = new PageProvider(host, httpClientConfigs);
+    this.pageProvider = createPageProvider(
+      host,
+      httpClientConfigs,
+      sessionConfigs
+    );
   }
 
   login({ username, password }) {
@@ -15,6 +41,12 @@ class RutrackerApi {
     return this.pageProvider
       .search({ query, sort, order })
       .then((html) => this.parser.parseSearch(html));
+  }
+
+  find(id) {
+    return this.pageProvider
+      .thread(id)
+      .then((html) => this.parser.parseThread(id, html));
   }
 
   download(id) {
